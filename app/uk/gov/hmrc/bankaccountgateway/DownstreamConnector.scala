593aabdb0f3d30b268d923b0c6fa59f2a66774e0
Pete Smith <pete@beyond-code.com>
2022-08-25 19:24:50 +1000

    [PS] TAV-606 - Use fresh header carrier to allow fine control over onward headers

--- app/uk/gov/hmrc/bankaccountgateway/DownstreamConnector.scala
+++ app/uk/gov/hmrc/bankaccountgateway/DownstreamConnector.scala
@@ -31,7 +31,7 @@ import scala.concurrent.{ExecutionContext, Future}
 class DownstreamConnector @Inject()(httpClient: HttpClient) {
   private val logger = Logger(this.getClass.getSimpleName)
 
-  def forward(request: Request[AnyContent], url: String)(implicit hc: HeaderCarrier, ec: ExecutionContext): Future[Result] = {
+  def forward(request: Request[AnyContent], url: String)(implicit ec: ExecutionContext): Future[Result] = {
     import uk.gov.hmrc.http.HttpReads.Implicits.readRaw
 
     logger.info(s"Forwarding to downstream url: $url")
@@ -39,6 +39,7 @@ class DownstreamConnector @Inject()(httpClient: HttpClient) {
     (request.method, request.headers(CONTENT_TYPE).toLowerCase()) match {
       case ("POST", "application/json") =>
         val onwardHeaders = request.headers.remove(CONTENT_LENGTH, HOST, AUTHORIZATION).headers
+        implicit val hc: HeaderCarrier = HeaderCarrier()
 
         try {
           httpClient.POST[Option[JsValue], HttpResponse](url = url, body = request.body.asJson, onwardHeaders)
