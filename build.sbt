cb8551cf0d4434d9e758ecf4399c26a4d2d4dadb
Saqib@Ee <saqib.saleem1@digital.hmrc.gov.uk>
2023-12-15 09:49:20 +0000

    CIR-1383: Play3 upgrade - deps cleanup

--- build.sbt
+++ build.sbt
@@ -9,18 +9,12 @@ lazy val microservice = Project(appName, file("."))
   .enablePlugins(play.sbt.PlayScala, SbtDistributablesPlugin)
   .settings(
     libraryDependencies              ++= AppDependencies.compile ++ AppDependencies.test,
-    // ***************
-    // Use the silencer plugin to suppress warnings
-//    scalacOptions += "-P:silencer:pathFilters=routes",
   )
   .settings(PlayKeys.playDefaultPort := 8345)
   .settings(resolvers += Resolver.jcenterRepo)
-  .settings(CodeCoverageSettings.settings: _*)
+  .settings(CodeCoverageSettings.settings*)
 
 lazy val it = project.in(file("it"))
   .enablePlugins(play.sbt.PlayScala)
   .dependsOn(microservice % "test->test")
   .settings(DefaultBuildSettings.itSettings)
-  .settings(
-    libraryDependencies ++= AppDependencies.itTest
-  )
--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,21 +1,14 @@
-import play.sbt.PlayImport._
-import sbt._
+import sbt.*
 
 object AppDependencies {
 
   private val bootstrapPlayVersion = "8.1.0"
 
-  val compile = Seq(
+  val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% "bootstrap-backend-play-30" % bootstrapPlayVersion,
   )
 
-  val test = Seq(
-    "uk.gov.hmrc" %% "bootstrap-test-play-30" % bootstrapPlayVersion % "test",
-    "com.vladsch.flexmark"    %  "flexmark-all"               % "0.36.8"            % "test"
-  )
-
-  val itTest = Seq(
-    "uk.gov.hmrc" %% "bootstrap-test-play-30" % bootstrapPlayVersion % "it",
-    "com.vladsch.flexmark"    %  "flexmark-all"               % "0.36.8"            % "it"
+  val test: Seq[ModuleID] = Seq(
+    "uk.gov.hmrc" %% "bootstrap-test-play-30" % bootstrapPlayVersion % Test
   )
 }
